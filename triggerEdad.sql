8629e5390e54f7e4b9ca9f87362e7cebdb8f5f78
biomecanica <biomecanica@biomecanica-PC.(none)>
2012-07-27 13:59:55 -0430

    Fue agregado el archivo que contiene el trigger para calcula la edad de los pacientes.

---
+++ triggerEdad.sql
@@ -0,0 +1,19 @@
+﻿drop trigger t_calcula_edad on persona;
+create or replace function calcula_edad()
+	returns trigger
+	language plpgsql
+	as
+$$
+DECLARE
+fechaact DATE;
+BEGIN
+	SELECT CURRENT_DATE into fechaact;
+	new.edad:=FLOOR(((DATE_PART('YEAR',fechaact)-DATE_PART('YEAR',new.fechanac))* 372 + (DATE_PART('MONTH',fechaact) - DATE_PART('MONTH',new.fechanac))*31 + (DATE_PART('DAY',fechaact)-DATE_PART('DAY',new.fechanac)))/372);
+	return new;
+END
+$$;
+
+create trigger t_calcula_edad
+	before insert on persona
+	for each row
+	execute procedure calcula_edad();
